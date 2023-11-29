03da81703c1c67902270c7427fa75d824e152686
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-11-29 07:42:25 +0600

    Deisy UI installed, React Icons Installed and create the add task field with one input field

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 
+import { FaPlus } from "react-icons/fa6";
+
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-5xl text-center ">This is my First Next Js Project</h1>
+    <main className="max-w-4xl mx-auto p-10">
+      <h1 className="text-2xl text-center font-bold">TODO List</h1>
+      <div className="flex justify-center">
+        <div className="flex mt-5">
+          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+          <button className="btn btn-neutral ms-3">Add Task <FaPlus /></button>
+        </div>
+      </div>
+
     </main>
   )
 }
